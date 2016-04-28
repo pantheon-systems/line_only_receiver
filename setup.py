7488fa1668b78dfdb9c59611df16b0aaa7655fa4
Jesse Nelson <spheromak@gmail.com>
2016-04-28 15:59:12 -0700

    initial commit of TCP Pool twistd client

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='line_only_receiver',
+    version='0.1',
+    url="https://github.com/pantheon-systems/line_only_receiver@master",
+    description='Twistd TCP Line Protocol Connection Pool',
+    long_description='',
+    author='Michael Liu',
+    author_email='michael.liu@getpantheon.com',
+    license='BSD',
+    keywords='Twistd TCP'.split(),
+    platforms='any',
+    include_package_data=False
+    #test_suite='test_dashvisor.run_tests.run_all',
+)
